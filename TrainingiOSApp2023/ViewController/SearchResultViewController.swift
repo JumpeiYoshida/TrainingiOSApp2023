caeb680b9fb32552e2c42506a111579038654834
JumpeiYoshida <jumpei.yoshida@axas-japan.co.jp>
2023-02-20 10:09:18 +0900

    modify: APIの結果をCellに反映し一覧画面に表示

--- TrainingiOSApp2023/ViewController/SearchResultViewController.swift
+++ TrainingiOSApp2023/ViewController/SearchResultViewController.swift
@@ -10,17 +10,31 @@ import UIKit
 final internal class SearchResultViewController: UIViewController {
 	
 	@IBOutlet private weak var tableView: UITableView!
+	
+	private var shopEntities = [GourmetSearchShopEntity]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 		navigationItem.title = "検索結果一覧画面"
 		
 		setupTableView()
+		fetchList()
     }
 	
 	private func setupTableView() {
 		tableView.delegate = self
 		tableView.dataSource = self
+		tableView.register(UINib(nibName: "SearchResultCell", bundle: nil), forCellReuseIdentifier: "SearchResultCell")
+	}
+	
+	private func fetchList() {
+		GourmetSearchFetcher().fetch { [weak self] entities in
+			guard let self else { return }
+			DispatchQueue.main.async {
+				self.shopEntities = entities
+				self.tableView.reloadData()
+			}
+		}
 	}
 }
 
@@ -29,15 +43,18 @@ extension SearchResultViewController: UITableViewDelegate {
 }
 
 extension SearchResultViewController: UITableViewDataSource {
-	func numberOfSections(in tableView: UITableView) -> Int {
-		1
-	}
-	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		1
+		shopEntities.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		return UITableViewCell(style: .default, reuseIdentifier: "tableViewCell")
+		guard let cell = tableView
+			.dequeueReusableCell(withIdentifier: "SearchResultCell", for: indexPath) as? SearchResultCell,
+			  shopEntities.count > indexPath.row else {
+			return UITableViewCell()
+		}
+		
+		cell.setup(entity: shopEntities[indexPath.row])
+		return cell
 	}
 }
